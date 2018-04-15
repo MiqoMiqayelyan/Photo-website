95829320e154802b24d7debdf3355fcb766fdb67
MiqoMiqayelyan <miqo.miqayelyan@mail.ru>
2018-04-15 15:55:24 +0400

    first paje

---
+++ js/main.js
@@ -0,0 +1,84 @@
+let show1 = document.getElementById('show1');
+show1.addEventListener('mouseenter', function(){
+	let showTop = document.getElementsByClassName('show_top');
+	let showLeft = document.getElementsByClassName('show_left');
+	let showDown = document.getElementsByClassName('show_down');
+	let showRight = document.getElementsByClassName('show_raight');
+	showTop[0].style.display = 'flex';
+	showLeft[0].style.display = 'flex';
+	showDown[0].style.display = 'flex';
+	showRight[0].style.display = 'flex';
+})
+show1.addEventListener('mouseleave', function(){
+	let showTop = document.getElementsByClassName('show_top');
+	let showLeft = document.getElementsByClassName('show_left');
+	let showDown = document.getElementsByClassName('show_down');
+	let showRight = document.getElementsByClassName('show_raight');
+	showTop[0].style.display = 'none';
+	showLeft[0].style.display = 'none';
+	showDown[0].style.display = 'none';
+	showRight[0].style.display = 'none';
+})
+let show2 = document.getElementById('show2');
+show2.addEventListener('mouseenter', function(){
+	let showTop1 = document.getElementsByClassName('show_top1');
+	let showLeft1 = document.getElementsByClassName('show_left1');
+	let showDown1 = document.getElementsByClassName('show_down1');
+	let showRight1 = document.getElementsByClassName('show_raight1');
+	showTop1[0].style.display = 'flex';
+	showLeft1[0].style.display = 'flex';
+	showDown1[0].style.display = 'flex';
+	showRight1[0].style.display = 'flex';
+})
+show2.addEventListener('mouseleave', function(){
+	let showTop1 = document.getElementsByClassName('show_top1');
+	let showLeft1 = document.getElementsByClassName('show_left1');
+	let showDown1 = document.getElementsByClassName('show_down1');
+	let showRight1 = document.getElementsByClassName('show_raight1');
+	showTop1[0].style.display = 'none';
+	showLeft1[0].style.display = 'none';
+	showDown1[0].style.display = 'none';
+	showRight1[0].style.display = 'none';
+})
+let show3 = document.getElementById('show3');
+show3.addEventListener('mouseenter', function(){
+	let showTop2 = document.getElementsByClassName('show_top2');
+	let showLeft2 = document.getElementsByClassName('show_left2');
+	let showDown2 = document.getElementsByClassName('show_down2');
+	let showRight2 = document.getElementsByClassName('show_raight2');
+	showTop2[0].style.display = 'flex';
+	showLeft2[0].style.display = 'flex';
+	showDown2[0].style.display = 'flex';
+	showRight2[0].style.display = 'flex';
+})
+show3.addEventListener('mouseleave', function(){
+	let showTop2 = document.getElementsByClassName('show_top2');
+	let showLeft2 = document.getElementsByClassName('show_left2');
+	let showDown2 = document.getElementsByClassName('show_down2');
+	let showRight2 = document.getElementsByClassName('show_raight2');
+	showTop2[0].style.display = 'none';
+	showLeft2[0].style.display = 'none';
+	showDown2[0].style.display = 'none';
+	showRight2[0].style.display = 'none';
+})
+let show4 = document.getElementById('show4');
+show4.addEventListener('mouseenter', function(){
+	let showTop3 = document.getElementsByClassName('show_top3');
+	let showLeft3 = document.getElementsByClassName('show_left3');
+	let showDown3 = document.getElementsByClassName('show_down3');
+	let showRight3 = document.getElementsByClassName('show_raight3');
+	showTop3[0].style.display = 'flex';
+	showLeft3[0].style.display = 'flex';
+	showDown3[0].style.display = 'flex';
+	showRight3[0].style.display = 'flex';
+})
+show4.addEventListener('mouseleave', function(){
+	let showTop3 = document.getElementsByClassName('show_top3');
+	let showLeft3 = document.getElementsByClassName('show_left3');
+	let showDown3 = document.getElementsByClassName('show_down3');
+	let showRight3 = document.getElementsByClassName('show_raight3');
+	showTop3[0].style.display = 'none';
+	showLeft3[0].style.display = 'none';
+	showDown3[0].style.display = 'none';
+	showRight3[0].style.display = 'none';
+})
